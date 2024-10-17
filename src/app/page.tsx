deeb8109afefd6efdbd8ce0ac29da0fca36d7c12
mkireva <mariya@kireva.de>
2024-10-17 17:45:43 +0200

    add: quote wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { UnderConstruction } from "@/components/under-construction";
-import { Quote } from "lucide-react";
-
 export default function Home() {
   return (
     <div className="h-screen">
